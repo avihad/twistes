68d6efa05963c08b22022de27dea405afe983b1a
Amit Yogev <amit@senexx.com>
2016-08-14 16:02:24 +0300

    aggregation helper methods
    * utilities tests - has aggregation results + extract aggregation results
    * relevant consts
    * tests

--- tests/test_esutils.py
+++ tests/test_esutils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from twistes.consts import EsConst, EsDocProperties
+from twistes.consts import EsConst, EsDocProperties, EsAggregation
 from twistes.exceptions import ScanError
 from twistes.utilities import EsUtils
 
@@ -51,3 +51,57 @@ class TestEsUtils(TestCase):
         es_get_response = {EsDocProperties.TYPE: "type", EsDocProperties.INDEX: "index", EsConst.FIELDS: {},
                            EsDocProperties.VERSION: 1, EsConst.FOUND: True, EsDocProperties.ID: "id"}
         self.assertTrue(EsUtils.is_get_query_with_results(es_get_response))
+
+    def test_has_aggregation_results_true(self):
+        agg_name = "results"
+        actual_results = [
+            {
+                EsAggregation.KEY: "https://www.linkedin.com/company/forward-philippines",
+                EsAggregation.DOC_COUNT: 1
+            }
+        ]
+        agg_results = self.create_agg_results(agg_name, actual_results)
+        result = EsUtils.has_aggregation_results(agg_results, agg_name)
+        self.assertTrue(result)
+
+    def test_has_aggregation_results_false(self):
+        agg_name = "results"
+        agg_results = self.create_agg_results(agg_name, [])
+        result = EsUtils.has_aggregation_results(agg_results, agg_name)
+        self.assertFalse(result)
+
+    def test_extract_aggregation_results_has_results(self):
+        agg_name = "results"
+        expected = [
+            {
+                EsAggregation.KEY: "https://www.linkedin.com/company/forward-philippines",
+                EsAggregation.DOC_COUNT: 1
+            }
+        ]
+        agg_results = self.create_agg_results(agg_name, expected)
+        result = EsUtils.extract_aggregation_results(agg_results, agg_name)
+        self.assertEquals(result, expected)
+
+    def test_extract_aggregation_results_no_results(self):
+        agg_name = "results"
+        expected = []
+        agg_results = self.create_agg_results(agg_name, expected)
+        result = EsUtils.extract_aggregation_results(agg_results, agg_name)
+        self.assertEquals(result, expected)
+
+    @staticmethod
+    def create_agg_results(agg_name, actual_results):
+        agg_results = {
+            EsConst.HITS: {
+                EsConst.TOTAL: 1,
+                EsConst.HITS: []
+            },
+            EsAggregation.AGGREGATIONS: {
+                agg_name: {
+                    "doc_count_error_upper_bound": 0,
+                    "sum_other_doc_count": 0,
+                    EsAggregation.BUCKETS: actual_results
+                }
+            }
+        }
+        return agg_results
