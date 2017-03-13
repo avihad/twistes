8f0ad99d7437596db71d97fbcca6e26f3511a139
Jona <jonathanfarache@gmail.com>
2017-03-13 15:11:26 +0200

    cr fixes

--- twistes/client.py
+++ twistes/client.py
@@ -29,8 +29,8 @@ class Elasticsearch(object):
     def __init__(self, hosts, timeout=10,
                  async_http_client=None,
                  async_http_client_params=None,
-                 retry_on_timeout=True,
-                 max_retries=0):
+                 retry_on_timeout=False,
+                 max_retries=3):
         self._es_parser = EsParser()
         self._hostname, self._auth = self._es_parser.parse_host(hosts)
         self._timeout = timeout
@@ -589,7 +589,9 @@ class Elasticsearch(object):
         returnValue(result)
 
     @inlineCallbacks
-    def _perform_request(self, method, path, body=None, params=None):
+    def _perform_request(self, method, path, body=None, params=None, num_retries=None):
+
+        num_retries = self._max_retries if num_retries is None else num_retries
         url = self._es_parser.prepare_url(self._hostname, path, params)
 
         if body is not None and not isinstance(body, string_types):
@@ -623,11 +625,9 @@ class Elasticsearch(object):
 
         except ResponseNeverReceived as e:
 
-            if self._retry_on_timeout and self._max_retries <= 3:
-                self._max_retries += 1
-                response = yield self._perform_request(method, path, body, params)
-                content = yield self._get_content(response)
-                returnValue(content)
+            if self._retry_on_timeout and num_retries >= 0:
+                response = yield self._perform_request(method, path, body, params, num_retries - 1)
+                returnValue(response)
 
             raise ConnectionTimeout(str(e))
 
